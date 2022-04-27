36451d9adcf74cc184d9bd9f73f2e00cb46821f5
MUAzizbek <mu.azizbek@gmail.com>
2022-04-27 10:55:54 +0500

    legaldesk html

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/* */
+
+document.getElementById("userAddUser").addEventListener("mouseenter", mouseEnter);
+        document.getElementById("userAddUser").addEventListener("mouseleave", mouseLeave);
+
+    function mouseEnter() {
+    document.getElementById("userAddUser").style.width = "max-content";
+    }
+
+    function mouseLeave() {
+    document.getElementById("userAddUser").style.width = "24px";
+    }
+
+
+  /*  */
+
+
+
