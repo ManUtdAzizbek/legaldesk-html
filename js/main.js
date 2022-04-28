743af11727e227da6f0ec13e3e79ed6674ba6551
MUAzizbek <mu.azizbek@gmail.com>
2022-04-28 12:14:44 +0500

    users edit modal

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-/* */
+window.onload=function(){
 
 document.getElementById("userAddUser").addEventListener("mouseenter", mouseEnter);
-        document.getElementById("userAddUser").addEventListener("mouseleave", mouseLeave);
-
+document.getElementById("userAddUser").addEventListener("mouseleave", mouseLeave);
+        
     function mouseEnter() {
     document.getElementById("userAddUser").style.width = "max-content";
     }
@@ -10,9 +10,50 @@ document.getElementById("userAddUser").addEventListener("mouseenter", mouseEnter
     function mouseLeave() {
     document.getElementById("userAddUser").style.width = "24px";
     }
+  }
+
+  /* Modal window */
 
 
-  /*  */
-
+  const modalTriggerButtons = document.querySelectorAll("[data-modal-target]");
+  const modals = document.querySelectorAll(".modal");
+  const modalCloseButtons = document.querySelectorAll(".modal-close");
+  
+  modalTriggerButtons.forEach(elem => {
+    elem.addEventListener("click", event => toggleModal(event.currentTarget.getAttribute("data-modal-target")));
+  });
+  modalCloseButtons.forEach(elem => {
+    elem.addEventListener("click", event => toggleModal(event.currentTarget.closest(".modal").id));
+  });
+  modals.forEach(elem => {
+    elem.addEventListener("click", event => {
+      if(event.currentTarget === event.target) toggleModal(event.currentTarget.id);
+    });
+  });
+  
+  // Close Modal
+  document.addEventListener("keydown", event => {
+    if(event.keyCode === 27 && document.querySelector(".modal.modal-show")) {
+      toggleModal(document.querySelector(".modal.modal-show").id);
+    }
+  });
+  
+  function toggleModal(modalId) {
+    const modal = document.getElementById(modalId);
+  
+    if(getComputedStyle(modal).display==="flex") {
+      modal.classList.add("modal-hide");
+      setTimeout(() => {
+        document.body.style.overflow = "initial";
+        modal.classList.remove("modal-show", "modal-hide");
+        modal.style.display = "none";      
+      }, 200);
+    }
+    else {
+      document.body.style.overflow = "hidden";
+      modal.style.display = "flex";
+      modal.classList.add("modal-show");
+    }
+  }
 
 
