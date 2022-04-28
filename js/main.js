99bb1d3cfc28c1ba8fc66502080f1586cf9ea1b3
MUAzizbek <mu.azizbek@gmail.com>
2022-04-28 17:51:50 +0500

    users edit modal

--- js/main.js
+++ js/main.js
@@ -57,3 +57,17 @@ document.getElementById("userAddUser").addEventListener("mouseleave", mouseLeave
   }
 
 
+/* */
+
+function toggleNav() {
+  const element = document.querySelector("nav");
+  const elementHeight = element.scrollHeight;
+  var isCollapsed = element.getAttribute('data-collapsed') === 'true';
+  if (isCollapsed) {
+    element.style.height = elementHeight + "px";
+    element.setAttribute("data-collapsed", "false");
+  } else {
+        element.style.height = "0px";
+    element.setAttribute("data-collapsed", "true");
+  }
+}
